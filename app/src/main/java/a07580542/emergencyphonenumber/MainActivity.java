f2a1823655c6ccb324aa821b0eb7b1b09bda3328
Dream D <dreamdayxiii@gmail.com>
2017-11-26 12:04:58 +0700

    commit naja

--- app/src/main/java/a07580542/emergencyphonenumber/MainActivity.java
+++ app/src/main/java/a07580542/emergencyphonenumber/MainActivity.java
@@ -4,13 +4,19 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import a07580542.emergencyphonenumber.db.PhonedbHelper;
+import a07580542.emergencyphonenumber.model.PhoneItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private PhonedbHelper pdbh;
     private SQLiteDatabase sqldb;
+    private ArrayList<PhoneItem> phoneitemslist = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
         pdbh = new PhonedbHelper(this);
         sqldb = pdbh.getReadableDatabase();
 
-        Cursor cursor = sqldb.query(pdbh.getTb_name(),null,null,null,null,null,null);
+        Cursor cursor = sqldb.query(PhonedbHelper.getTb_name(),null,null,null,null,null,null);
 
         while(cursor.moveToNext()){
-            String title = cursor.getString(cursor.getColumnIndex("title"));
+            String title = cursor.getString(cursor.getColumnIndex(PhonedbHelper.getCol_title()));
+            String number = cursor.getString(cursor.getColumnIndex(PhonedbHelper.getCol_number()));
+            String picture = cursor.getString(cursor.getColumnIndex(PhonedbHelper.getCol_picture()));
+            int id = cursor.getInt(cursor.getColumnIndex(PhonedbHelper.getCol_id()));
+            PhoneItem item = new PhoneItem(id,title,number,picture);
+            phoneitemslist.add(item);
         }
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,phoneitemslist);
+        ListView lv = findViewById(R.id.listview);
+        lv.setAdapter(adapter);
     }
 }
