f92cc591eff651fd8a6fc59dd0a80e113fc9ab18
Dream D <dreamdayxiii@gmail.com>
2017-12-03 12:02:09 +0700

    commit naja

--- app/src/main/java/a07580542/emergencyphonenumber/AddPhoneActivity.java
+++ app/src/main/java/a07580542/emergencyphonenumber/AddPhoneActivity.java
@@ -1,5 +1,7 @@
 package a07580542.emergencyphonenumber;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.util.List;
+
+import pl.aprilapps.easyphotopicker.DefaultCallback;
 import pl.aprilapps.easyphotopicker.EasyImage;
 
 public class AddPhoneActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText phonetitle,phonenumber;
     private ImageView phoneimage;
     private Button phoneadd;
+
+    private File img_selected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +43,24 @@ public class AddPhoneActivity extends AppCompatActivity implements View.OnClickL
      }else if(viewid==R.id.phone_add_button){
 
      }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                //Some error handling
+            }
+
+            @Override
+            public void onImagesPicked(List<File> imagesFiles, EasyImage.ImageSource source, int type) {
+                //Handle the images
+                img_selected = imagesFiles.get(0);
+                Drawable drawable = Drawable.createFromPath(img_selected.getAbsolutePath());
+                phoneimage.setImageDrawable(drawable);
+            }
+        });
     }
 }
